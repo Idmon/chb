e1877f5d448988d76f8e472ef20f2bb91b0e3fa1
Idmon <idmon92@gmail.com>
2023-08-12 05:13:26 +0200

    Fix

--- chatbot/salesgpt/customLLM.py
+++ chatbot/salesgpt/customLLM.py
@@ -30,7 +30,7 @@ class customChatLLM(LLM):
             'max_new_tokens': 300,
             'auto_max_new_tokens': False,
             'mode': 'chat',  # Valid options: 'chat', 'chat-instruct', 'instruct'
-            'character': 'LUNAv4',
+            'character': 'LUNAv2',
             'instruction_template': 'Alpaca',  # Will get autodetected if unset
             # 'your_name': 'Idmon',
             # 'name1': 'Idmon', # Optional
@@ -49,29 +49,29 @@ class customChatLLM(LLM):
 
             # Generation params. If 'preset' is set to different than 'None', the values
             # in presets/preset-name.yaml are used instead of the individual numbers.
-            'preset': 'None',
-            'do_sample': True,
-            'temperature': 0.7,
-            'top_p': 0.1,
-            'typical_p': 1,
-            'epsilon_cutoff': 0,  # In units of 1e-4
-            'eta_cutoff': 0,  # In units of 1e-4
-            'tfs': 1,
-            'top_a': 0,
-            'repetition_penalty': 1.18,
-            'repetition_penalty_range': 0,
-            'top_k': 40,
-            'min_length': 0,
-            'no_repeat_ngram_size': 0,
-            'num_beams': 1,
-            'penalty_alpha': 0,
-            'length_penalty': 1,
-            'early_stopping': False,
-            'mirostat_mode': 0,
-            'mirostat_tau': 5,
-            'mirostat_eta': 0.1,
-            'guidance_scale': 1,
-            'negative_prompt': '',
+            'preset': 'simple-1',
+            # 'do_sample': True,
+            # 'temperature': 0.7,
+            # 'top_p': 0.1,
+            # 'typical_p': 1,
+            # 'epsilon_cutoff': 0,  # In units of 1e-4
+            # 'eta_cutoff': 0,  # In units of 1e-4
+            # 'tfs': 1,
+            # 'top_a': 0,
+            # 'repetition_penalty': 1.18,
+            # 'repetition_penalty_range': 0,
+            # 'top_k': 40,
+            # 'min_length': 0,
+            # 'no_repeat_ngram_size': 0,
+            # 'num_beams': 1,
+            # 'penalty_alpha': 0,
+            # 'length_penalty': 1,
+            # 'early_stopping': False,
+            # 'mirostat_mode': 0,
+            # 'mirostat_tau': 5,
+            # 'mirostat_eta': 0.1,
+            # 'guidance_scale': 1,
+            # 'negative_prompt': '',
 
             'seed': -1,
             'add_bos_token': True,
